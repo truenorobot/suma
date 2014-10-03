72684d34e351a5130333747d45da1894301bf40c
jacquelinemonterubio <jacko_fleyker@hotmail.com>
2014-10-02 19:11:39 -0500

    Base del proyecto

---
+++ suma/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 02/10/2014
+ * Time: 07:07 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace suma
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
